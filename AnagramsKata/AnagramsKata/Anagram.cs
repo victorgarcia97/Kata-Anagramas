dde30fb51dbe1b634cf9d91fffae5b74f81109d1
Victor Garcia <vgarcia@aidacanarias.com>
2023-05-23 16:53:00 +0200

    Refactor: Unify if statements

--- AnagramsKata/AnagramsKata/Anagram.cs
+++ AnagramsKata/AnagramsKata/Anagram.cs
@@ -4,9 +4,7 @@ public class Anagram
 {
     public static List<string> Of(string s)
     {
-        if (s == "") return new List<string> { "" };
-
-        if(s.Length == 1 ) return new List<string> { s };
+        if(s.Length <= 1 ) return new List<string> { s };
 
         if(s.Length == 2 ) return new List<string> { s, s.Substring(1,1) + s.Substring(0,1) };
 
